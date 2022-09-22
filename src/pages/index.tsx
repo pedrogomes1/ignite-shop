7683bff354cb502b312503eba80bdf197e387d22
Pedro Gomes <pedro-gomes@ibm.com>
2022-09-21 21:58:50 -0300

    feat: added content to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,30 @@
-import Head from "next/head";
 import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import shirt1 from "../assets/shirt/1.png";
+import shirt2 from "../assets/shirt/2.png";
+import shirt3 from "../assets/shirt/3.png";
 
 export default function Home() {
-  return <h1>asdasdsa</h1>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={400} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt1} width={520} height={400} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
