b8b6d0524cd3779ed3d29d416071a1a83df6aa01
Pedro Gomes <pedro-gomes@ibm.com>
2022-09-21 22:09:34 -0300

    chore: added keen slider carousel to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import shirt1 from "../assets/shirt/1.png";
 import shirt2 from "../assets/shirt/2.png";
 import shirt3 from "../assets/shirt/3.png";
 
+import "keen-slider/keen-slider.min.css";
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={400} alt="" />
 
         <footer>
@@ -17,7 +27,25 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt2} width={520} height={400} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={400} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={400} alt="" />
 
         <footer>
