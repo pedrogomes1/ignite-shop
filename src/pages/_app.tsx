53e7cc8afa45d9ce23b109ed532a4b676006b785
Pedro Gomes <pedro-gomes@ibm.com>
2022-09-21 21:29:46 -0300

    chore: added Header of pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
 
-export default MyApp;
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Ignite shop logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
+}
