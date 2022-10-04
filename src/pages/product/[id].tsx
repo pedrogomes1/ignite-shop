c758e71edeab7d73786ede04a5c4177a18369eea
Pedro Gomes <pedro-gomes@ibm.com>
2022-10-04 20:52:26 -0300

    chore: finish dynamic product page functions

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,30 +1,108 @@
-import { useRouter } from "next/router";
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
+import Stripe from "stripe";
+import { stripe } from "../../../lib/stripe";
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from "../../styles/pages/product";
+import { useState } from "react";
+import Head from "next/head";
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    imageUrl: string;
+    price: string;
+    defaultPriceId: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false);
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+      const response = await axios.post("/api/checkout", {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (error) {
+      alert("Falha ao redirecionar ao checkout");
+      setIsCreatingCheckoutSession(false);
+    }
+  }
 
   return (
-    <ProductContainer>
-      <ImageContainer></ImageContainer>
+    <>
+      <Head>
+        <title>Home | Ignite shop</title>
+      </Head>
 
-      <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        </ImageContainer>
 
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
-          aliquid rerum exercitationem facere a molestiae ut sed velit non
-          mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed
-          laboriosam tempora?
-        </p>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+          <p>{product.description}</p>
 
-        <button>Comprar agora</button>
-      </ProductDetails>
-    </ProductContainer>
+          <button
+            disabled={isCreatingCheckoutSession}
+            onClick={handleBuyProduct}
+          >
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_MTwnZLpY6ypGAg" } }],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const ONE_HOUR = 60 * 60 * 1;
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      },
+      revalidate: ONE_HOUR,
+    },
+  };
+};
